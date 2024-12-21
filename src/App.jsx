76a06d4984f97d1368c582f645c085a204bfddf8
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2024-12-21 03:14:37 +0100

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-	return <div className="container"></div>;
+	return (
+		<>
+			<div className="uppercase">Hello Tailwind</div>
+		</>
+	);
 };
 
 export default App;
