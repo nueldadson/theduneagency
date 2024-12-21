c3be512f239fb0019a39fa18ee761f13738a8a24
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2024-12-21 02:29:10 +0100

    Basic Seo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-	return (
-		<div className="container">
-			<h1>This Website is Undergoing Maintenance</h1>
-			<p>We would be back soon!</p>
-			<button className="smiley"></button>
-		</div>
-	);
+	return <div className="container"></div>;
 };
 
 export default App;
